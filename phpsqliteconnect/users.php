db67a2fa6ca3d9c81fc1f42fe5912160e35eb64d
Puja Karmacharya <pujakarmacharya17@gmail.com>
2022-06-19 22:36:03 +0200

    deleted commits

--- phpsqliteconnect/users.php
+++ phpsqliteconnect/users.php
@@ -4,21 +4,10 @@
 use App\Config;
 require 'vendor/autoload.php';
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,        // enable PDO errors
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,   // fetch associative arrays by default
-    PDO::ATTR_EMULATE_PREPARES => false,                // Use native prepared statements
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,        
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,   
+    PDO::ATTR_EMULATE_PREPARES => false,                
 ];
-//  
-// use App\SQLiteConnection;
-// $pdo = (new SQLiteConnection())->connect();
-// if ($pdo != null)
-//     echo 'Connected to the SQLite database successfully!';
-// else
-//     echo 'Whoops, could not connect to the SQLite database!';
-//     // handle the exception here  
-
-
-
 
 
 
@@ -40,37 +29,3 @@ function allusers() {
 }
 
 print allusers();
-// class index {
-
-//     function openDb() {    
-//         try {
-//             $this->pdo = new \PDO("sqlite:" . Config::PATH_TO_SQLITE_FILE);
-//         } catch (PDOException $e) {
-//             die("error, please try again");
-//         }        
-//        echo("successfullyconnected");
-//     }
-
-//     function getAllData($qty) {
-//         //prepared query to prevent SQL injections
-//         $query = "select * from TABLE where qty = ?";
-//         $stmt = $this->openDb()->prepare($query);
-//         $stmt->bindValue(1, $qty, PDO::PARAM_INT);
-//         $stmt->execute();
-//         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//         return $rows;
-//     }   
-
-
-// }?>
-
-<!-- <html>
-   <body>
-   
-      <form action = "<?php $_PHP_SELF ?>" method = "GET">
-         id: <input type = "number" name = "id" />
-         <input type = "submit" />
-      </form>
-      
-   </body>
-</html> -->
